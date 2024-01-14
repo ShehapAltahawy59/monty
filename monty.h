ebc0c75c9c4be4897ee16c5f6b3bc8aced9b565c
shehap62 <shehapkhalil62@gmail.com>
2024-01-14 13:59:23 +0200

    edit

--- monty.h
+++ monty.h
@@ -42,7 +42,7 @@ void parse_line(char *buffer,int line_number,int format);
 void find_opcode(char *opcode,unsigned int value_number,int line__number,int format);
 stack_t *head = NULL;
 void add_to_stack(stack_t **head,unsigned int value);
-stack_t *create_node(int value);
+stack_t *create_node(unsigned int value);
 void print_stack(stack_t **head, unsigned int none);
 
 
