c874cbe7ccc178451ccb85333a7118e579dd0a5f
shehap62 <shehapkhalil62@gmail.com>
2024-01-14 14:41:15 +0200

    edit

--- monty.h
+++ monty.h
@@ -1,11 +1,12 @@
 #ifndef MONTY_H
 #define MONTY_H
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <stdarg.h>
+#include <stdarg.h> 
 
 
 /**
