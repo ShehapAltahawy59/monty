4522c7be24616e0c2b067b0e5bcad3e931e0455a
shehap62 <shehapkhalil62@gmail.com>
2024-01-18 02:57:31 +0200

    edit

--- swap.c
+++ swap.c
@@ -13,7 +13,7 @@ void swap(stack_t **head,unsigned int line__number)
 	
 	if(pointer == NULL || pointer->prev == NULL)
 	{
-		fprintf(stderr,"L%d: cant swap, stack too short\n",line__number);
+		fprintf(stderr,"L%d: can't swap, stack too short\n",line__number);
 		free_nodes(head);
 		exit(EXIT_FAILURE);
 	}
