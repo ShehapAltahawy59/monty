099e5d9e06bb8a3dc5e52e5a4a1d40a50879fef2
shehap62 <shehapkhalil62@gmail.com>
2024-01-18 10:17:02 +0200

    edit

--- main.c
+++ main.c
@@ -22,3 +22,49 @@ int main(int argc, char *argv[])
 	return (0);
 }
 
+/**
+ * create_node - crate a new node
+ * @head: pointer of pointer to head of stack
+ * Return:pointer to node
+*/
+stack_t *create_node( int n)
+{
+	stack_t *new_node;
+	
+	new_node = malloc(sizeof(stack_t));
+	
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_nodes();
+		exit(EXIT_FAILURE);
+	}
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	new_node->n = n;
+	return (new_node);
+}
+
+/**
+ * free_nodes - print the head of stack
+ * @head: pointer of pointer to head of stack
+ * Return:void
+*/
+void free_nodes()
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
+
+
+
