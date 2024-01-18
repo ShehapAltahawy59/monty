aeb8acc78a28ea9b13a35ecc41be6cf678bfe573
shehap62 <shehapkhalil62@gmail.com>
2024-01-18 10:12:44 +0200

    edit

--- monty.h
+++ monty.h
@@ -1,6 +1,6 @@
 #ifndef MONTY_H
 #define MONTY_H
-#define _XOPEN_SOURCE 700
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
@@ -38,13 +38,14 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
 extern stack_t *head;
-void open_file(char *filename,stack_t **head );
-void read_file(FILE *fd,stack_t **head);
-int parse_line(char *buffer,unsigned int line_number,int format, stack_t **head);
-void find_opcode(char *opcode,char *value_number,unsigned int line__number,int format, stack_t **head);
+void open_file(char *filename);
+void read_file(FILE *fd);
+int parse_line(char *buffer,unsigned int line_number,int format);
+void find_opcode(char *opcode,char *value_number,unsigned int line__number,int format);
 void add_to_stack(stack_t **head,unsigned int line__number);
-stack_t *create_node(stack_t **head);
+stack_t *create_node( int n);
 void print_stack(stack_t **head, unsigned int none);
 void pint(stack_t **head,unsigned int line__number);
 void pop(stack_t **head,unsigned int line__number);
@@ -54,7 +55,7 @@ void nop(stack_t **head,unsigned int line__number);
 void err(int error_code, ...);
 void more_err(int error_code, ...);
 void string_err(int error_code, ...);
-void free_nodes(stack_t **head);
+void free_nodes();
 char *trim_whitespace(char *str);
 void sub(stack_t **head,unsigned int line__number);
 void div_(stack_t **head,unsigned int line__number);
@@ -64,7 +65,6 @@ void pchar(stack_t **head,unsigned int line__number);
 void pstr(stack_t **head, unsigned int none);
 void rotl(stack_t **head, unsigned int none);
 void rotr(stack_t **head, unsigned int none);
-void call_fun(op_func func, char *op, char *val, unsigned int ln, int format);
-
+void call_fun(instruction_t func, char *opcode, char *value_number, unsigned int line__number, int format);
  #endif
 
