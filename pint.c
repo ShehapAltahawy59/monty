735657b2d94ae9504f3e2d34035647f606e9035c
shehap62 <shehapkhalil62@gmail.com>
2024-01-18 11:45:09 +0200

    edit

--- pint.c
+++ pint.c
@@ -5,12 +5,11 @@
  * @line__number: the line which is excuted
  * Return:void
 */
-void pint(stack_t **new_node,unsigned int line__number)
+void pint(stack_t **new_node, unsigned int line__number)
 {
-    if (new_node == NULL || *new_node == NULL)
+	if (new_node == NULL || *new_node == NULL)
 	{
 		more_err(6, line__number);
 	}
 	printf("%d\n", (*new_node)->n);
-	
 }
