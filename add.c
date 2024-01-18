272eafff61aa468774350b038464231d7e253d5c
ManarAbdelhalimMohamed <145897768+ManarAbdelhalimMohahmed@users.noreply.github.com>
2024-01-18 22:01:20 +0200

    add.c

--- add.c
+++ add.c
@@ -15,7 +15,6 @@ void add(stack_t **new_node, unsigned int line__number)
 		more_err(8, line__number, "add");
 	}
 
-	
 	(*new_node) = (*new_node)->next;
 	sum = (*new_node)->n + (*new_node)->prev->n;
 	(*new_node)->n = sum;
