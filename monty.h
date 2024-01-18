263a2cb294f22929262d20786286a39efcb7e5cf
shehap62 <shehapkhalil62@gmail.com>
2024-01-18 08:19:55 +0200

    edit

--- monty.h
+++ monty.h
@@ -9,7 +9,6 @@
 #include <ctype.h>
 #include <stdarg.h>
 
-
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -17,53 +16,69 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void open_file(char *filename,stack_t **head );
-void read_file(FILE *fd,stack_t **head);
-int parse_line(char *buffer,unsigned int line_number,int format, stack_t **head);
-void find_opcode(char *opcode,char *value_number,unsigned int line__number,int format, stack_t **head);
-void add_to_stack(stack_t **head,unsigned int line__number);
-stack_t *create_node(stack_t **head);
-void print_stack(stack_t **head, unsigned int none);
-void pint(stack_t **head,unsigned int line__number);
-void pop(stack_t **head,unsigned int line__number);
-void swap(stack_t **head,unsigned int line__number);
-void add(stack_t **head,unsigned int line__number);
-void nop(stack_t **head,unsigned int line__number);
+extern stack_t *head;
+typedef void (*op_func)(stack_t **, unsigned int);
+
+/*file operations*/
+void open_file(char *file_name);
+int parse_line(char *buffer, int line_number, int format);
+void read_file(FILE *);
+int len_chars(FILE *);
+void find_func(char *, char *, int, int);
+
+/*Stack operations*/
+stack_t *create_node(int n);
+void free_nodes(void);
+void print_stack(stack_t **, unsigned int);
+void add_to_stack(stack_t **, unsigned int);
+void add_to_queue(stack_t **, unsigned int);
+
+void call_fun(op_func, char *, char *, int, int);
+
+void print_top(stack_t **, unsigned int);
+void pop_top(stack_t **, unsigned int);
+void nop(stack_t **, unsigned int);
+void swap_nodes(stack_t **, unsigned int);
+
+/*Math operations with nodes*/
+void add_nodes(stack_t **, unsigned int);
+void sub_nodes(stack_t **, unsigned int);
+void div_nodes(stack_t **, unsigned int);
+void mul_nodes(stack_t **, unsigned int);
+void mod_nodes(stack_t **, unsigned int);
+
+/*String operations*/
+void print_char(stack_t **, unsigned int);
+void print_str(stack_t **, unsigned int);
+void rotl(stack_t **, unsigned int);
+
+/*Error hanlding*/
 void err(int error_code, ...);
 void more_err(int error_code, ...);
 void string_err(int error_code, ...);
-void free_nodes(stack_t **head);
-char *trim_whitespace(char *str);
-void sub(stack_t **head,unsigned int line__number);
-void div_(stack_t **head,unsigned int line__number);
-void mul_(stack_t **head,unsigned int line__number);
-void mod_(stack_t **head,unsigned int line__number);
-void pchar(stack_t **head,unsigned int line__number);
-void pstr(stack_t **head, unsigned int none);
-void rotl(stack_t **head, unsigned int none);
-void rotr(stack_t **head, unsigned int none);
+void rotr(stack_t **, unsigned int);
 
- #endif
-
+#endif
